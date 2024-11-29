7b0a57c873abfc51b3be074ccc08d06c8b5e388d
vovila <vovila2003@yandex.ru>
2024-11-29 20:09:36 +0300

    Do cookbook and cookbook context via generic

--- Assets/Game/Scripts/Cooking/Recipes/DishCookbookContext.cs
+++ Assets/Game/Scripts/Cooking/Recipes/DishCookbookContext.cs
@@ -1,52 +1,8 @@
-using System.Collections.Generic;
-using Sirenix.OdinInspector;
-using UnityEngine;
+using Tavern.Common;
 
 namespace Tavern.Cooking
 {
-    public class DishCookbookContext : MonoBehaviour
+    public class DishCookbookContext : CookbookContext<DishItem>
     {
-        private DishCookbook _dishCookbook;
-
-        [SerializeField]
-        private DishRecipe[] DishRecipes;
-        
-        [ShowInInspector, ReadOnly]
-        private IReadOnlyDictionary<string, DishRecipe> Recipes => _dishCookbook?.Recipes; 
-
-        private void Awake()
-        {
-            _dishCookbook = new DishCookbook(DishRecipes);
-        }
-
-        [Button]
-        public void AddRecipe(DishRecipe recipe)
-        {
-            bool result = _dishCookbook.AddRecipe(recipe);
-            if (!result)
-            {
-                Debug.Log("Add recipe to cookbook: FAIL");
-            }
-        }
-
-        [Button]
-        public void RemoveRecipe(DishRecipe recipe)
-        {
-            bool result = _dishCookbook.RemoveRecipeByConfig(recipe);
-            if (!result)
-            {
-                Debug.Log("Remove recipe from cookbook: FAIL");
-            }
-        }
-
-        [Button]
-        public void RemoveRecipe(string recipeName)
-        {
-            bool result = _dishCookbook.RemoveRecipeByName(recipeName);
-            if (!result)
-            {
-                Debug.Log("Remove recipe from cookbook: FAIL");
-            }
-        }
     }
 }
--- Assets/Game/Scripts/Gardening/Fertilizer/Recipes/FertilizerCookbookContext.cs
+++ Assets/Game/Scripts/Gardening/Fertilizer/Recipes/FertilizerCookbookContext.cs
@@ -1,54 +1,8 @@
-using System.Collections.Generic;
-using Sirenix.OdinInspector;
-using Tavern.Cooking;
-using UnityEngine;
-using UnityEngine.Serialization;
+using Tavern.Common;
 
 namespace Tavern.Gardening.Fertilizer
 {
-    public class FertilizerCookbookContext : MonoBehaviour
+    public class FertilizerCookbookContext : CookbookContext<FertilizerItem>
     {
-        private FertilizerCookbook _fertilizerCookbook;
-
-        [SerializeField]
-        private FertilizerRecipe[] FertilizerRecipes;
-        
-        [ShowInInspector, ReadOnly]
-        private IReadOnlyDictionary<string, FertilizerRecipe> Recipes => _fertilizerCookbook?.Recipes; 
-
-        private void Awake()
-        {
-            _fertilizerCookbook = new FertilizerCookbook(FertilizerRecipes);
-        }
-
-        [Button]
-        public void AddRecipe(FertilizerRecipe recipe)
-        {
-            bool result = _fertilizerCookbook.AddRecipe(recipe);
-            if (!result)
-            {
-                Debug.Log("Add recipe to cookbook: FAIL");
-            }
-        }
-
-        [Button]
-        public void RemoveRecipe(FertilizerRecipe recipe)
-        {
-            bool result = _fertilizerCookbook.RemoveRecipeByConfig(recipe);
-            if (!result)
-            {
-                Debug.Log("Remove recipe from cookbook: FAIL");
-            }
-        }
-
-        [Button]
-        public void RemoveRecipe(string recipeName)
-        {
-            bool result = _fertilizerCookbook.RemoveRecipeByName(recipeName);
-            if (!result)
-            {
-                Debug.Log("Remove recipe from cookbook: FAIL");
-            }
-        }
     }
 }
